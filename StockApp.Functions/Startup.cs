10f14e89a70a549449f50da47da5f9118064b5e5
rdw100 <ryandustinwright@hotmail.com>
2024-09-03 10:33:31 -0400

    Add project files.

---
+++ StockApp.Functions/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StockApp.Shared;
+using System;
+using System.Net.Http;
+
+[assembly: FunctionsStartup(typeof(StockApp.Functions.Startup))]
+
+namespace StockApp.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(configuration);
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<IStockService, MarketStockService>(sp =>
+            {
+                var config = sp.GetRequiredService<IConfiguration>();
+                var httpClient = sp.GetRequiredService<IHttpClientFactory>().CreateClient();
+                return new MarketStockService(httpClient, config);
+            });
+        }
+    }
+}
---
+++ StockApp.Shared/IStockService.cs
@@ -0,0 +1,8 @@
+﻿namespace StockApp.Shared
+{
+    public interface IStockService
+    {
+        string ServiceName { get; }
+        Task<ChartResult> GetStockPriceAsync(string symbol);
+    }
+}
