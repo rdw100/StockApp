4a6529e8284d12ca1a6a7fcf45db69a43ef34129
rdw100 <ryandustinwright@hotmail.com>
2024-09-03 14:57:05 -0400

    Added new wasm project for build

---
+++ StockApp.Blazor/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using StockApp.Blazor;
+using StockApp.Shared;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["MySettings:API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IStockService, MarketStockService>();
+
+await builder.Build().RunAsync();
