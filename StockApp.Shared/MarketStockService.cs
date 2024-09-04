0119e169c135a9cfc7cf30421c37866fff11e30c
rdw100 <ryandustinwright@hotmail.com>
2024-09-04 12:24:15 -0400

    Added shared library removal

--- StockApp.Shared/MarketStockService.cs
+++ StockApp.Shared/MarketStockService.cs
@@ -7,25 +7,25 @@
 
     public class MarketStockService : IStockService
     {
-        private readonly HttpClient _httpClient;
-        private readonly string _apiRootUrl;
-        private HttpClient httpClient;
+        //private readonly HttpClient _httpClient;
+        //private readonly string _apiRootUrl;
+        //private HttpClient httpClient;
 
-        public MarketStockService(HttpClient httpClient)
-        {
-            _httpClient = httpClient;
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
-            _apiRootUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
-        }
+        //public MarketStockService(HttpClient httpClient)
+        //{
+        //    _httpClient = httpClient;
+        //    _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+        //    _apiRootUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
+        //}
 
-        public string ServiceName => "Market Stock API";
+        //public string ServiceName => "Market Stock API";
 
-        public async Task<ChartResult> GetStockPriceAsync(string symbol)
-        {
-            var response = await _httpClient.GetAsync($"{_apiRootUrl}{symbol}?interval=1d&range=5d");
-            response.EnsureSuccessStatusCode();
-            var result = await response.Content.ReadFromJsonAsync<ChartResult>();
-            return result;
-        }
+        //public async Task<ChartResult> GetStockPriceAsync(string symbol)
+        //{
+        //    var response = await _httpClient.GetAsync($"{_apiRootUrl}{symbol}?interval=1d&range=5d");
+        //    response.EnsureSuccessStatusCode();
+        //    var result = await response.Content.ReadFromJsonAsync<ChartResult>();
+        //    return result;
+        //}
     }
 }
