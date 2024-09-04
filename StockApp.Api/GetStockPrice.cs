1df5a8c608fde25b0c9db8017aae6c5b231bffff
rdw100 <ryandustinwright@hotmail.com>
2024-09-04 15:00:59 -0400

    Added try catch back

--- StockApp.Api/GetStockPrice.cs
+++ StockApp.Api/GetStockPrice.cs
@@ -15,8 +15,8 @@ public static class GetStockPrice
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
         ILogger log)
     {
-        //try
-        //{
+        try
+        {
             string apiUrl = "https://query1.finance.yahoo.com/v8/finance/chart/";
             string symbol = req.Query["symbol"];
             _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
@@ -24,11 +24,11 @@ public static class GetStockPrice
             response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadFromJsonAsync<ChartResult>();
             return new OkObjectResult(result);
-        //}
-        //catch (Exception ex)
-        //{
-        //    log.LogError($"Error fetching AAPL stock data: {ex.Message}");
-        //    return new StatusCodeResult(500);
-        //}
+        }
+        catch (Exception ex)
+        {
+            log.LogError($"Error fetching AAPL stock data: {ex.Message}");
+            return new StatusCodeResult(500);
+        }
     }
 }
