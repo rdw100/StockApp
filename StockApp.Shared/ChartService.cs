a6e1f85a9a7aaddea314d4eafab01650891d5da6
rdw100 <ryandustinwright@hotmail.com>
2024-09-13 16:19:42 -0400

    Added callable service using DI to pass configuration settings via constructor

--- StockApp.Shared/ChartService.cs
+++ StockApp.Shared/ChartService.cs
@@ -1,14 +1,28 @@
-﻿namespace StockApp.Shared
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace StockApp.Shared
 {
     public class ChartService : IChartService
     {
-        public ChartService() { }
-
         private static readonly HttpClient httpClient = new HttpClient();
+        private readonly string baseUrl;
 
-        public Task<ChartResult> GetStockPriceAsync(string symbol, string interval, string range)
+        public ChartService(IOptions<ChartServiceOptions> options)
         {
-            throw new NotImplementedException();
+            baseUrl = options.Value.BaseUrl;
+        }
+
+        public async Task<ChartResult> GetStockPriceAsync(string symbol, string interval, string range)
+        {
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+            var url = $"{baseUrl}{symbol}?interval={interval}&range={range}";
+            //var chartResult = await httpClient.GetFromJsonAsync<ChartResult>(url);
+
+            var response = await httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            var chartResult = await response.Content.ReadFromJsonAsync<ChartResult>();
+            return chartResult;
         }
     }
 }
