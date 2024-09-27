c355cb351448209feece9dce1939f93bcf0015dd
rdw100 <ryandustinwright@hotmail.com>
2024-09-27 11:00:14 -0400

    Added new WASM Client

--- StockApp.Client/Program.cs
+++ StockApp.Client/Program.cs
@@ -2,14 +2,13 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using StockApp.Client;
-using StockApp.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["MySettings:API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
-builder.Services.AddScoped<IStockService, MarketStockService>();
 
 await builder.Build().RunAsync();
