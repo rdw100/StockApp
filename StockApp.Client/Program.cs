216b0129a359ec6fd5059351d231e7a650ec0aaa
rdw100 <ryandustinwright@hotmail.com>
2024-09-28 17:34:43 -0400

    Added local settings prefix

--- StockApp.Client/Program.cs
+++ StockApp.Client/Program.cs
@@ -10,7 +10,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["MySettings:API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, UserAuthenticationStateProvider>();
