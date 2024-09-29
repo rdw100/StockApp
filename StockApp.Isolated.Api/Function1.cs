b4d2ea66fcb0befacde82f38b8a944d9e2b2034c
rdw100 <ryandustinwright@hotmail.com>
2024-09-29 11:39:12 -0400

    Added hello to new api

--- StockApp.Isolated.Api/Function1.cs
+++ StockApp.Isolated.Api/Function1.cs
@@ -31,5 +31,25 @@ namespace StockApp.Isolated.Api
 
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("Hello")]
+        public static async Task<IActionResult> RunHello(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered HELLO function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
     }
 }
