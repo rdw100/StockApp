1a1c0d85b8e698fb0e971edaa014ccd22e6d29fd
rdw100 <ryandustinwright@hotmail.com>
2024-09-29 12:49:10 -0400

    Added 8 migration async host, function logger, & prefix.  Cleared obj and bin to work.

--- StockApp.Isolated.Api/Function1.cs
+++ StockApp.Isolated.Api/Function1.cs
@@ -3,37 +3,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace StockApp.Isolated.Api
 {
-    public static class Function1
+    public class Function1
     {
-        [Function("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        private readonly ILogger<Function1> _logger;
+
+        public Function1(ILogger<Function1> logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            _logger = logger;
         }
 
         [Function("Hello")]
-        public static async Task<IActionResult> RunHello(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello")] HttpRequest req,
-            ILogger log)
+        public async Task<IActionResult> RunHello(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hello")] HttpRequest req)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
 
--- StockApp.Isolated.Api/Program.cs
+++ StockApp.Isolated.Api/Program.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+    })
     .Build();
 
-host.Run();
+await host.RunAsync();
