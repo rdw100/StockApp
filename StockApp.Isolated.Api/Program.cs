1a38538c58dee22a8e55eab1ba14ac911a6cb81d
rdw100 <ryandustinwright@hotmail.com>
2024-09-30 07:30:03 -0400

    Added base for third party api retrieval

--- StockApp.Isolated.Api/Program.cs
+++ StockApp.Isolated.Api/Program.cs
@@ -1,10 +1,28 @@
 ﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StockApp.Api.Services;
+using StockApp.Api.Services.Http;
+using StockApp.Api.Services.Interfaces;
+using StockApp.Shared;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        // Add configuration sources (environment variables, local.settings.json, etc.)
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true);
+        config.AddEnvironmentVariables();
+    })
+    .ConfigureServices((context, services) => 
+    {
+        services.AddHttpClient();
+        services.AddMemoryCache();
+        services.Configure<ServiceOptions>(context.Configuration.GetSection("ThirdPartyApi"));
+        services.AddSingleton<IFreeHttpService, FreeHttpService>();
+        services.AddSingleton<IChartService, ChartService>();
+        services.AddSingleton<IQuoteService, QuoteService>();
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
     })
