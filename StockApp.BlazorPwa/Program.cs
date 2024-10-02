b91c1d8a5f0df94f1a9b009e57b26f2b5aec8d04
rdw100 <ryandustinwright@hotmail.com>
2024-10-02 08:10:57 -0400

    Added WASM PWA parity with WASM project

--- StockApp.BlazorPwa/Program.cs
+++ StockApp.BlazorPwa/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
-//using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using StockApp.BlazorPwa;
@@ -11,19 +10,12 @@ using StockApp.BlazorPwa.Services;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, UserAuthenticationStateProvider>();
-//builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-//                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-//builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ServerAPI"));
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["MySettings:API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, UserAuthenticationStateProvider>();
 builder.Services.AddScoped<IChartService, ChartService>();
 builder.Services.AddScoped<IQuoteService, QuoteService>();
-//builder.Services.AddApiAuthorization();
-//builder.Services.AddOptions();
-//builder.Services.AddScoped<AuthenticationStateProvider, UserAuthenticationStateProvider>();
 builder.Services.AddFluentUIComponents();
 
 await builder.Build().RunAsync();
